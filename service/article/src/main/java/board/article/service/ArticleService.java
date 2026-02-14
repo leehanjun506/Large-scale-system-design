51ab1fba7046512cd490d955db2ecc4a815f6c92
hanjun <sis03017@naver.com>
2026-02-14 16:43:59 +0900

    feat : create infinity scroll api

--- service/article/src/main/java/board/article/service/ArticleService.java
+++ service/article/src/main/java/board/article/service/ArticleService.java
@@ -11,6 +11,8 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.List;
+
 @Service
 @RequiredArgsConstructor
 public class ArticleService {
@@ -45,6 +47,13 @@ public class ArticleService {
                         .stream()
                         .map(ArticleResponse::from).toList(),
                 articleRepository.count(boardId, PageLimitCalculator.calculatePageLimit(page, pageSize, 10L)));
+    }
+
+    public List<ArticleResponse> readAllInfiniteScroll(Long boardId, Long pageSize, Long lastArticleId) {
+        List<Article> articles = lastArticleId == null ?
+                articleRepository.findAllInfiniteScroll(boardId, pageSize) :
+                articleRepository.findAllInfiniteScroll(boardId, pageSize, lastArticleId);
+        return articles.stream().map(ArticleResponse::from).toList();
     }
 
 }
--- service/article/src/test/java/board/article/repository/ArticleRepositoryTest.java
+++ service/article/src/test/java/board/article/repository/ArticleRepositoryTest.java
@@ -8,8 +8,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 
 import java.util.List;
 
-import static org.junit.jupiter.api.Assertions.*;
-
 @Slf4j
 @SpringBootTest
 class ArticleRepositoryTest {
@@ -29,6 +27,21 @@ class ArticleRepositoryTest {
     void countTest() {
         Long count = articleRepository.count(1L, 10000L);
         log.info("count = {}",count);
+    }
+
+    @Test
+    void findInfiniteScrollTest() {
+        List<Article> articles = articleRepository.findAllInfiniteScroll(1L, 30L);
+        for (Article article : articles) {
+            log.info("articleId = {}", article.getArticleId());
+        }
+
+        Long lastArticleId = articles.get(articles.size()-1).getArticleId();
+        List<Article> articles2 = articleRepository.findAllInfiniteScroll(1L, 30L, lastArticleId);
+        for (Article article : articles2) {
+            log.info("articleId = {}", article.getArticleId());
+        }
+
     }
 
 }
