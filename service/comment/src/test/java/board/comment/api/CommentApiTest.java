b4f48500a6f1abafd3b45a1ea0cac5f0034fe23b
이한준 <hj.lee3@okestro.com>
2026-08-26 17:10:37 +0900

    feat: add comment service

---
+++ service/comment/src/test/java/board/comment/api/CommentApiTest.java
@@ -0,0 +1,67 @@
+package board.comment.api;
+
+import board.comment.service.response.CommentResponse;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import org.junit.jupiter.api.Test;
+import org.springframework.web.client.RestClient;
+
+public class CommentApiTest {
+    RestClient restClient = RestClient.create("http://localhost:9001");
+
+    @Test
+    void create() {
+        CommentResponse response1 = createComment(new CommentCreateRequest(1L, "my comment1", null, 1L));
+        CommentResponse response2 = createComment(new CommentCreateRequest(1L, "my comment2", response1.getCommentId(), 1L));
+        CommentResponse response3 = createComment(new CommentCreateRequest(1L, "my comment3", response1.getCommentId(), 1L));
+
+        System.out.printf("commentId=%s".formatted(response1.getCommentId()));
+        System.out.printf("\tcommentId=%s%n", response2.getCommentId());
+        System.out.printf("\tcommentId=%s%n", response3.getCommentId());
+
+//        commentId=349075522236534784
+//	commentId=349075522785988608
+//	commentId=349075522857291776
+
+    }
+
+    CommentResponse createComment(CommentCreateRequest request) {
+        return restClient.post()
+                .uri("/v1/comments")
+                .body(request)
+                .retrieve()
+                .body(CommentResponse.class);
+    }
+
+    @Test
+    void read() {
+        CommentResponse response = restClient.get()
+                .uri("/v1/comments/{commentId}", 349075522236534784L)
+                .retrieve()
+                .body(CommentResponse.class);
+
+        System.out.println("response = " + response);
+    }
+
+    @Test
+    void delete() {
+        //        commentId=349075522236534784
+//	commentId=349075522785988608
+//	commentId=349075522857291776
+
+        restClient.delete()
+                .uri("/v1/comments/{commentId}", 349075522857291776L)
+                .retrieve()
+                .toBodilessEntity();
+    }
+
+    @Getter
+    @AllArgsConstructor
+    public static class CommentCreateRequest {
+        private Long articleId;
+        private String content;
+        private Long parentCommentId;
+        private Long writerId;
+    }
+
+}
