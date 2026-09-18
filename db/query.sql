4d630f3cd237e32b3e99eeff8ef21c223ed53c06
이한준 <hj.lee3@okestro.com>
2026-09-18 11:18:02 +0900

    feat: add Redis-backed article view counts

--- db/query.sql
+++ db/query.sql
@@ -126,3 +126,10 @@ article_id bigint not null primary key,
 comment_count bigint not null
 );
 
+create database article_view;
+use article_view;
+create table article_view_count (
+article_id bigint not null primary key,
+view_count bigint not null
+);
+
